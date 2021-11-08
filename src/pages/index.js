7f93c956f3c0894a95e4658349824339dcd72391
piducancore:/# <hola@piducan.dev>
2021-11-08 11:14:09 -0300

    redirect to first verse

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,6 @@
-/** @jsx jsx */
-import { jsx, Themed, Flex } from "theme-ui"
+import { navigate } from "gatsby"
 
-const IndexPage = () => (
-  <Flex sx={{ height: "100vh", width: "100vw", p: 3, overflowX: "auto" }}>
-    <div sx={{ m: "auto" }}>
-      <Themed.h1
-        sx={{ fontFamily: "cover", my: 0, textAlign: "center", fontSize: 9 }}
-      >
-        ∞
-      </Themed.h1>
-      <Themed.h1 sx={{ fontFamily: "cover", my: 0, textAlign: "center" }}>
-        versos chilenos
-      </Themed.h1>
-    </div>
-  </Flex>
-)
-
-export default IndexPage
+export default function Index() {
+  navigate("/1")
+  return null
+}
