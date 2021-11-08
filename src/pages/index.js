b3f0f273d2a02230583f709d8c6469fdf64b6fa9
piducancore:/# <hola@piducan.dev>
2021-11-08 19:16:05 -0300

    use seo component in index and 404 pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+import React, { useEffect } from "react"
 import { navigate } from "gatsby"
 
-export default function Index() {
-  if (typeof window !== "undefined")
+import Seo from "../components/seo"
+
+export default function Index(props) {
+  useEffect(() =>
     navigate(
       "/dulce-patria-recibe-los-votos-con-que-chile-en-tus-aras-juro-que-o-la-tumba-seras-de-los-libres-o-el-asilo-contra-la-opresion"
     )
-  return null
+  )
+  return <Seo title="" />
 }
