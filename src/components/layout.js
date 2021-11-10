b96910f23f5bb7f36546802f1fdafa30cab91b3b
piducancore:/# <hola@piducan.dev>
2021-11-10 17:05:48 -0300

    add verse to state list onStepEnter

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx, Themed, Spinner, Flex } from "theme-ui"
-import React, { useEffect } from "react"
+import React from "react"
 import { Scrollama, Step } from "react-scrollama"
 import { navigate } from "gatsby"
 import { useList } from "../useList"
@@ -9,8 +9,7 @@ import Seo from "./seo"
 
 export default function Layout({ pageContext }) {
   const { list, add } = useList()
-  useEffect(() => add(pageContext), [add, pageContext])
-
+  const onStepEnter = () => add(pageContext)
   const onStepExit = ({ direction }) => {
     if (direction === "down") {
       const next = createPath(pageContext.next.verso)
@@ -23,39 +22,39 @@ export default function Layout({ pageContext }) {
   }
 
   return (
-    <main sx={{ width: "100%", flex: "1 1 auto", variant: "layout.main" }}>
+    <React.Fragment>
       <Seo title={pageContext.verso} />
-      <Scrollama onStepExit={onStepExit} offset={0}>
-        <Step data={{ hola: "mundo" }}>
-          <div sx={{ height: "10vh" }}></div>
-        </Step>
-        {list.map(({ verso, autor, fuente }, stepIndex) => (
-          <Step data={{ verso, autor, fuente }} key={stepIndex}>
-            <Flex sx={{ minHeight: "80vh", width: "100%", p: 3 }}>
-              <div sx={{ m: "auto" }}>
-                <pre
-                  sx={{
-                    fontSize: 6,
-                    fontFamily: "heading",
-                    whiteSpace: "break-spaces",
-                  }}
-                >
-                  {verso}
-                </pre>
-                <Themed.p>
-                  {autor ? autor : "Autor desconocido"}
-                  {fuente ? (
-                    <React.Fragment>, «{fuente}»</React.Fragment>
-                  ) : null}
-                </Themed.p>
-              </div>
-            </Flex>
+      <main sx={{ variant: "layout.main" }}>
+        <Scrollama onStepExit={onStepExit} onStepEnter={onStepEnter} offset={0}>
+          <Step data={{ hola: "mundo" }}>
+            <div sx={{ height: "10vh" }}></div>
           </Step>
-        ))}
-      </Scrollama>
-      <Flex sx={{ height: "60vh", textAlign: "center" }}>
-        <Spinner sx={{ m: "auto" }} />
-      </Flex>
-    </main>
+          {list.map(({ verso, autor, fuente }, stepIndex) => (
+            <Step data={{ verso, autor, fuente }} key={stepIndex}>
+              <Flex sx={{ minHeight: "80vh", width: "100%", p: 3 }}>
+                <div sx={{ m: "auto" }}>
+                  <pre
+                    sx={{
+                      fontSize: 6,
+                      fontFamily: "heading",
+                      whiteSpace: "break-spaces",
+                    }}
+                  >
+                    {verso}
+                  </pre>
+                  <Themed.p>
+                    {autor ? autor : "Autor desconocido"}
+                    {fuente ? <React.Fragment>, «{fuente}»</React.Fragment> : null}
+                  </Themed.p>
+                </div>
+              </Flex>
+            </Step>
+          ))}
+        </Scrollama>
+        <Flex sx={{ height: "60vh", textAlign: "center" }}>
+          <Spinner sx={{ m: "auto" }} />
+        </Flex>
+      </main>
+    </React.Fragment>
   )
 }
