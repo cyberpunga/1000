353b8b282540e225e2a47ef2fa742c73527264c8
piducancore:/# <hola@piducan.dev>
2021-11-11 12:11:48 -0300

    create form component

--- src/components/form.js
+++ src/components/form.js
@@ -36,39 +36,62 @@ export default function Form() {
   }
 
   return (
-    <Box
-      ref={ref}
-      as="form"
-      sx={{
-        variant: "forms.primary",
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-      }}
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-    >
-      <Label htmlFor="autor">Autor</Label>
-      <Input type="text" name="autor" id="autor" required />
-      <Label htmlFor="fuente">Fuente</Label>
-      <Input type="text" name="fuente" id="fuente" required />
-      <Label htmlFor="verso">Verso</Label>
-      <Textarea name="verso" id="verso" rows={6} required />
-      <Flex sx={{ mt: 3, height: "20vh" }}>
-        {loading ? (
-          <Spinner color="secondary" sx={{ m: "auto" }} />
-        ) : success ? (
-          <div>
-            <Themed.p sx={{ fontFamily: "heading", fontSize: 5 }}>
-              Gracias por tu aporte!
-              <br />
-              Para agregar más versos a la lista <Themed.a onClick={() => setSuccess(false)}>clickea aquí</Themed.a>.
-            </Themed.p>
-          </div>
-        ) : (
-          <Button sx={{ mx: "auto", mb: "auto", width: "100%", fontSize: 5, fontFamily: "heading" }}>Agregar a la lista</Button>
-        )}
-      </Flex>
-    </Box>
+    <div sx={{ maxWidth: 640, py: 6, px: 3, mx: "auto" }}>
+      <Themed.h1
+        sx={{
+          mb: 5,
+          fontSize: 5,
+          fontWeight: 800,
+          textAlign: "center",
+        }}
+      >
+        Último aviso
+      </Themed.h1>
+      <Themed.p
+        sx={{
+          fontSize: 5,
+          fontFamily: "heading",
+          textAlign: "justify",
+        }}
+      >
+        Como sabemos que los lectores echarán en falta más de algún verso inolvidable, hemos dejado{" "}
+        {/* <del>estas hojas en blanco</del> */} <span sx={{ fontFamily: "body" }}>el siguiente formulario</span> para que los
+        anoten y los sumen a una lista que ya imaginamos interminable.
+      </Themed.p>
+      <Box
+        ref={ref}
+        as="form"
+        sx={{
+          variant: "forms.primary",
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+        }}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      >
+        <Label htmlFor="autor">Autor</Label>
+        <Input type="text" name="autor" id="autor" required />
+        <Label htmlFor="fuente">Fuente</Label>
+        <Input type="text" name="fuente" id="fuente" required />
+        <Label htmlFor="verso">Verso</Label>
+        <Textarea name="verso" id="verso" rows={6} required />
+        <Flex sx={{ mt: 3, height: "20vh" }}>
+          {loading ? (
+            <Spinner color="secondary" sx={{ m: "auto" }} />
+          ) : success ? (
+            <div>
+              <Themed.p sx={{ fontFamily: "heading", fontSize: 5 }}>
+                Gracias por tu aporte!
+                <br />
+                Para agregar más versos a la lista <Themed.a onClick={() => setSuccess(false)}>clickea aquí</Themed.a>.
+              </Themed.p>
+            </div>
+          ) : (
+            <Button sx={{ mx: "auto", mb: "auto", width: "100%", fontSize: 5, fontFamily: "body" }}>Agregar a la lista</Button>
+          )}
+        </Flex>
+      </Box>
+    </div>
   )
 }
--- src/pages/agregar.js
+++ src/pages/agregar.js
@@ -1,33 +1,9 @@
 /** @jsx jsx */
-import { jsx, Themed } from "theme-ui"
+import { jsx } from "theme-ui"
 import Form from "../components/form"
 
 function Agregar() {
-  return (
-    <div sx={{ maxWidth: 640, py: 6, px: 3, mx: "auto" }}>
-      <Themed.h1
-        sx={{
-          mb: 5,
-          fontSize: 5,
-          fontWeight: 800,
-          textAlign: "center",
-        }}
-      >
-        Último aviso
-      </Themed.h1>
-      <Themed.p
-        sx={{
-          fontSize: 5,
-          fontFamily: "heading",
-        }}
-      >
-        Como sabemos que los lectores echarán en falta más de algún verso inolvidable, hemos dejado{" "}
-        <del>estas hojas en blanco</del> este formulario para que los anoten y los sumen a una lista que ya imaginamos
-        interminable.
-      </Themed.p>
-      <Form />
-    </div>
-  )
+  return <Form />
 }
 
 export default Agregar
